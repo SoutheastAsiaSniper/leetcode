738749122d41947c1e998e4a9abeb4640e3a76f0
liuwenjie <2335457193@qq.com>
2020-11-02 22:26:47 +0800

    20201102

--- src/Test.java
+++ src/Test.java
@@ -3,4 +3,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Test {
+    public static void main(String[] args) {
+        String[] s = new String[]{"a","b","c","d","e"};
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(i);
+            System.out.println(s[++i]);
+            System.out.println(i);
+        }
+    }
 }
